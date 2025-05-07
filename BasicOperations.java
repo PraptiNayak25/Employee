62fe467e627f90816bb9aa483d99c436c9d2ccc6
prapticnayak@gmail.com <prapticnayak@gmail.com>
2025-05-07 17:32:27 +0530

    save

---
+++ BasicOperations.java
@@ -0,0 +1,20 @@
+package Operations;
+
+public class BasicOperations {
+    
+        public static void main(String[] args) {
+            int num1 = 10;
+            int num2 = 5;
+    
+            int sum = num1 + num2;
+            int difference = num1 - num2;
+            int product = num1 * num2;
+    
+            System.out.println("Number 1: " + num1);
+            System.out.println("Number 2: " + num2);
+            System.out.println("Addition: " + sum);
+            System.out.println("Subtraction: " + difference);
+            System.out.println("Multiplication: " + product);
+        }
+    }
+    
